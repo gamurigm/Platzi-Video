1adeaa45b2f092aea3615f0aca47239fad10328e
Gabriel Murillo Medina <gamurigm@gmail.com>
2020-06-22 14:27:40 -0500

    Creacion de componentes: Header, Search y sus respectivos estilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const loader = require('sass-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js', /*referencia al archivo principal*/
@@ -25,6 +27,16 @@ module.exports = {
           loader: "html-loader"/* Loader de html */
         },
       },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ],
   }, /* Dicta las reglas necesarias para el proyecto */
   plugins: [
@@ -32,5 +44,8 @@ module.exports = {
       template: './public/index.html', /* Donde esta ubicado el template */
       filename: './index.html'/* Nombre */
     }),
-  ], /* Añada los plugins que se necesitan */
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
+    }),
+  ], /* Añada los plugins que se necesitan antes de este corchete */
 };
